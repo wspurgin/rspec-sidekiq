d9f7627d57fd34d4d63081ffa34aab89d909a4f4
Phil Ostler <philostler@gmail.com>
2013-03-25 01:31:02 +0000

    Update versions to 0.2.0

--- lib/rspec/sidekiq/version.rb
+++ lib/rspec/sidekiq/version.rb
@@ -1,5 +1,5 @@
 module RSpec
   module Sidekiq
-    VERSION = "0.1.0"
+    VERSION = "0.2.0"
   end
 end
--- spec/rspec/sidekiq/version_spec.rb
+++ spec/rspec/sidekiq/version_spec.rb
@@ -3,5 +3,5 @@ require "spec_helper"
 describe "Version" do
   subject { RSpec::Sidekiq::VERSION }
 
-  expect_it { to eq("0.1.0") }
+  expect_it { to eq("0.2.0") }
 end
