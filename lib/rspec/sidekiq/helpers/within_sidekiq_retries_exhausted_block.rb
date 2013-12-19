c5c6fbd31c00f4111b393e6d94950f549df88127
Gabe Heafitz <gabe@shopkeep.com>
2013-12-19 11:41:02 -0500

    Add helper for testing retries exhausted block.
    The block sidekiq_retries_exhausted currently lacks an easy way to kick
    it off and test it in RSpec. Add a helper that will kick off the block
    in order to test what happens within it.

---
+++ lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
@@ -0,0 +1,14 @@
+module Sidekiq
+  module Worker
+
+    module ClassMethods
+      def within_sidekiq_retries_exhausted_block(&block)
+        block.call
+
+        self.sidekiq_retries_exhausted_block.call
+      end
+    end
+
+  end
+end
+
