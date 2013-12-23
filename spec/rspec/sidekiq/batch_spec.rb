5de181a54ec17a6d1801bf86383bcea230dd89ca
Kelly Felkins <pair+kelly@pacerpro.com>
2013-12-23 14:45:54 -0800

    Add implementation of status.total

---
+++ spec/rspec/sidekiq/batch_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe "Batch" do
+
+  module Sidekiq
+    module Batch
+      class Status
+      end
+    end
+  end
+
+  load File.expand_path(File.join(File.dirname(__FILE__), "../../../lib/rspec/sidekiq/batch.rb"))
+
+  describe "NullStatus" do
+    describe "#total" do
+      it "returns 0 when no jobs" do
+        null_status = RSpec::Sidekiq::NullStatus.new
+        expect(null_status.total).to eq(0)
+      end
+
+      it "returns 1 when 1 job" do
+        null_status = RSpec::Sidekiq::NullStatus.new
+        TestWorker.perform_async('5')
+        expect(null_status.total).to eq(1)
+      end
+    end
+  end
+end
