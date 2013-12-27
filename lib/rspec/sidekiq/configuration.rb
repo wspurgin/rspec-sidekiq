05e8b6bbf01d937916d05591a1db2edbd5882c08
Phil Ostler <philostler@gmail.com>
2013-12-27 00:24:59 +0000

    Adds warning when jobs not processed by Sidekiq. Closes #33

--- lib/rspec/sidekiq/configuration.rb
+++ lib/rspec/sidekiq/configuration.rb
@@ -1,10 +1,12 @@
 module RSpec
   module Sidekiq
     class Configuration
-      attr_accessor :clear_all_enqueued_jobs
+      attr_accessor :clear_all_enqueued_jobs, :enable_terminal_colours, :warn_when_jobs_not_processed_by_sidekiq
 
       def initialize
         @clear_all_enqueued_jobs = true
+        @enable_terminal_colours = true
+        @warn_when_jobs_not_processed_by_sidekiq = true
       end
     end
   end
--- lib/rspec/sidekiq/sidekiq.rb
+++ lib/rspec/sidekiq/sidekiq.rb
@@ -15,6 +15,12 @@ module RSpec
 end
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    message = "[rspec-sidekiq] WARNING! Sidekiq will *NOT* process jobs in this environment. See https://github.com/philostler/rspec-sidekiq/wiki/FAQ-&-Troubleshooting"
+    message = "\e[33m#{message}\e[0m" if RSpec::Sidekiq.configuration.enable_terminal_colours
+    puts message if RSpec::Sidekiq.configuration.warn_when_jobs_not_processed_by_sidekiq
+  end
+
   config.before(:each) do
     Sidekiq::Worker.clear_all if RSpec::Sidekiq.configuration.clear_all_enqueued_jobs
   end
