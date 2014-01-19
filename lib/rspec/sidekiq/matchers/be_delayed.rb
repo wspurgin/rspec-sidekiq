16f8a4e8e5244a70932445f18561a6ef1ff8e383
Phil Ostler <philostler@gmail.com>
2014-01-19 18:32:19 +0000

    Majorly refactors be_a_delayed_job matcher. Closes #25
    Renamed to be_delayed with greatly enhanced matching capability

--- lib/rspec/sidekiq/matchers/be_delayed.rb
+++ lib/rspec/sidekiq/matchers/be_delayed.rb
@@ -1,61 +1,68 @@
 module RSpec
   module Sidekiq
     module Matchers
-      def be_delayed delay_time = nil
-        BeDelayed.new delay_time
+      def be_delayed *expected_arguments
+        BeDelayed.new *expected_arguments
       end
 
       class BeDelayed
-        def initialize delay_time = nil
-          @delay_time = delay_time
+        def initialize *expected_arguments
+          @expected_arguments = expected_arguments
         end
 
         def description
-          message = "have an delayed job #{@method}"
-          message += " with delay time #{@delay_time}" if @delay_time
+          description = "be delayed"
+          description += " for #{@expected_interval} seconds" if @expected_interval
+          description += " until #{@expected_time}" if @expected_time
+          description += " with arguments #{@expected_arguments}" unless @expected_arguments.empty?
+          description
         end
 
         def failure_message
-          message = "expected to have a delayed job #{@method}"
-          message += " with delay time #{@delay_time}" if @delay_time
-          message += " but it was not found\n\nfound: #{@actual_job_names}"
+          message = "expected #{@expected_method.receiver}.#{@expected_method.name} to be delayed"
+          message += " for #{@expected_interval} seconds" if @expected_interval
+          message += " until #{@expected_time}" if @expected_time
+          message += " with arguments #{@expected_arguments}" unless @expected_arguments.empty?
+          message
         end
 
-        def matches? expected
-          @expected = expected
+        def for interval
+          @expected_interval = interval
+          self
+        end
+
+        def matches? expected_method
+          @expected_method = expected_method
 
           job = (::Sidekiq::Extensions::DelayedClass.jobs + ::Sidekiq::Extensions::DelayedModel.jobs + ::Sidekiq::Extensions::DelayedMailer.jobs).find do |job|
             yaml = YAML.load(job["args"].first)
-
-            clazz = yaml[0]
-            method = yaml[1]
-            arguments = yaml[2]
-
-            expected.receiver == clazz && expected.name == method
+            @expected_method.receiver == yaml[0] && @expected_method.name == yaml[1] && (@expected_arguments <=> yaml[2]) == 0
           end
 
           if job
-            return true
+            if @expected_interval
+              return job["at"].to_i == job["enqueued_at"].to_i + @expected_interval
+            elsif @expected_time
+              return job["at"].to_i == @expected_time.to_i
+            else
+              return true
+            end
           else
             return false
           end
-
-#          time_matched = true
-#          if matched_job && @delay_time
-#            planned_run_time = matched_job['at']
-#            enqueued_at = matched_job['enqueued_at']
-
-#            return false if planned_run_time.nil? || enqueued_at.nil?
-
-#            time_matched = Time.at(planned_run_time).to_i == (Time.at(enqueued_at) + @delay_time).to_i
-#          end
-
-#          return time_matched
         end
 
         def negative_failure_message
-          message = "expected to not have a delayed job #{@method}"
-          message += " with delay time #{@delay_time}" if @delay_time
+          message = "expected #{@expected_method.receiver}.#{@expected_method.name} to not be delayed"
+          message += " for #{@expected_interval} seconds" if @expected_interval
+          message += " until #{@expected_time}" if @expected_time
+          message += " with arguments #{@expected_arguments}" unless @expected_arguments.empty?
+          message
+        end
+
+        def until time
+          @expected_time = time
+          self
         end
       end
     end
