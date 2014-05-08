0bed36ec249e9990c2fa9fa92054710fba423248
Ryan LeFevre <meltingice8917@gmail.com>
2014-05-08 11:33:10 -0400

    Pass message hash to retries_exhausted block

--- lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
+++ lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
@@ -1,10 +1,18 @@
 module Sidekiq
   module Worker
     module ClassMethods
-      def within_sidekiq_retries_exhausted_block &block
+      def within_sidekiq_retries_exhausted_block user_msg = {}, &block
         block.call
+        self.sidekiq_retries_exhausted_block.call default_retries_exhausted_args.merge(user_msg)
+      end
 
-        self.sidekiq_retries_exhausted_block.call
+      def default_retries_exhausted_args
+        {
+          'queue' => get_sidekiq_options[:worker],
+          'class' => self.name,
+          'args' => [],
+          'error_message' => 'An error occured'
+        }
       end
     end
   end
