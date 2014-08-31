84a497d74446038e70741f5c06b92a22ac352c13
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2014-08-31 15:54:38 -0700

    Remove Ruby 1.9.3 which is not supported with Sidekiq 3.x.  Fix failing spec issues.

--- spec/rspec/sidekiq/matchers/be_unique_spec.rb
+++ spec/rspec/sidekiq/matchers/be_unique_spec.rb
@@ -31,9 +31,9 @@ describe RSpec::Sidekiq::Matchers::BeUnique do
         end
       end
 
-      describe '#negative_failure_message' do
+      describe '#failure_message_when_negated' do
         it 'returns message' do
-          expect(subject.negative_failure_message).to eq "expected #{@worker} to not be unique in the queue"
+          expect(subject.failure_message_when_negated).to eq "expected #{@worker} to not be unique in the queue"
         end
       end
     end
@@ -57,7 +57,7 @@ describe RSpec::Sidekiq::Matchers::BeUnique do
 
   describe '#failure_message_when_negated' do
     it 'returns message' do
-      expect(subject.failure_message_when_negated).to eq "expected #{worker} to not be unique in the queue"
+      expect(subject.failure_message_when_negated).to eq "expected #{@worker} to not be unique in the queue"
     end
   end
 end
