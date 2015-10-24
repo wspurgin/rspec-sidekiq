109a21de37a3c84bad564189218773728c642c1b
Wanderson Policarpo <wpolicarpo@gmail.com>
2015-10-24 00:51:50 -0200

    Improve code coverage

--- spec/rspec/sidekiq/batch_spec.rb
+++ spec/rspec/sidekiq/batch_spec.rb
@@ -10,30 +10,61 @@ RSpec.describe 'Batch' do
 
   load File.expand_path(File.join(File.dirname(__FILE__), '../../../lib/rspec/sidekiq/batch.rb'))
 
+  describe 'NullObject' do
+    describe '#method_missing' do
+      it 'returns itself' do
+        batch = Sidekiq::Batch.new
+        expect(batch.non_existent_method).to eq(batch)
+      end
+    end
+  end
+
+  describe 'NullBatch' do
+  end
+
   describe 'NullStatus' do
+    let(:batch) {  Sidekiq::Batch.new }
+
+    subject { batch.status }
+
     describe '#total' do
       it 'returns 0 when no jobs' do
-        null_status = Sidekiq::Batch.new.status
-        expect(null_status.total).to eq(0)
+        expect(subject.total).to eq(0)
       end
 
       it 'returns 1 when 1 job' do
-        batch = Sidekiq::Batch.new
-
         batch.jobs do
           TestWorker.perform_async('5')
         end
 
-        null_status = batch.status
+        expect(subject.total).to eq(1)
+      end
+    end
 
-        expect(null_status.total).to eq(1)
+    describe '#failures' do
+      it 'returns 0' do
+        expect(subject.failures).to eq(0)
       end
     end
 
     describe '#bid' do
       it 'returns a bid' do
-        null_status = Sidekiq::Batch.new
-        expect(null_status.bid).to_not be_nil
+        expect(subject.bid).to_not be_nil
+      end
+    end
+
+    describe '#join' do
+      class MyCallback
+        def on_event(status, options); end
+      end
+
+      before(:each) do
+        batch.on(:event, MyCallback, my_arg: 42)
+      end
+
+      it 'executes callbacks' do
+        expect_any_instance_of(MyCallback).to receive(:on_event).with(subject, { my_arg: 42 })
+        subject.join
       end
     end
   end
