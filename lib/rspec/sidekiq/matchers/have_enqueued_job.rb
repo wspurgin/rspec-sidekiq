e9b119030ea7a8b9bd3316399626c99ea1ffde9e
Trevor Wistaff <trev@a07.com.au>
2017-05-23 15:20:40 +1000

    Make deprecation warning actionable

--- lib/rspec/sidekiq/matchers/have_enqueued_job.rb
+++ lib/rspec/sidekiq/matchers/have_enqueued_job.rb
@@ -2,13 +2,12 @@ module RSpec
   module Sidekiq
     module Matchers
       def have_enqueued_job(*expected_arguments)
+        if __callee__ == :have_enqueued_job && Gem::Dependency.new('rspec-rails', '>= 3.4.0').matching_specs.max_by(&:version)
+          warn "[DEPRECATION] `have_enqueued_job` is deprecated.  Please use `have_enqueued_sidekiq_job` instead."
+        end
         HaveEnqueuedJob.new expected_arguments
       end
-
-      if Gem::Dependency.new('rspec-rails', '>= 3.4.0').matching_specs.max_by(&:version)
-        warn "[DEPRECATION] `have_enqueued_job` is deprecated.  Please use `have_enqueued_sidekiq_job` instead."
-        alias have_enqueued_sidekiq_job have_enqueued_job
-      end
+      alias :have_enqueued_sidekiq_job :have_enqueued_job
 
       class JobOptionParser
         attr_reader :job
