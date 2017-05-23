b19e0157afd7ef80cf879f8effe8d1d2e0c6136c
Trevor Wistaff <trev@a07.com.au>
2017-05-23 16:22:52 +1000

    Make test pass again due to changes in Sidekiq 5+ (https://github.com/mperham/sidekiq/wiki/Delayed-extensions)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'rspec-sidekiq'
 
 require 'active_job'
 require 'action_mailer'
+require 'active_record'
 
 require_relative 'support/init'
 
@@ -17,8 +18,12 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
-
   config.include RSpec::Sidekiq::Spec::Support::Factories
 end
 
 ActiveJob::Base.queue_adapter = :sidekiq
+
+if Gem::Dependency.new('sidekiq', '>= 5.0.0').matching_specs.any?
+  ActiveSupport.run_load_hooks(:active_record, ActiveRecord::Base)
+  Sidekiq::Extensions.enable_delay!
+end
