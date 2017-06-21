b2426eebf7da34aa1de20df3b8101447af9faf37
Alexander Cederblad <alexcederblad@gmail.com>
2017-06-21 13:19:27 +0200

    Fix ::Fixnum deprecation warning

--- spec/rspec/sidekiq/matchers/have_enqueued_job_spec.rb
+++ spec/rspec/sidekiq/matchers/have_enqueued_job_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RSpec::Sidekiq::Matchers::HaveEnqueuedJob do
   let(:tomorrow) { DateTime.now + 1 }
   let(:interval) { 3.minutes }
   let(:argument_subject) { RSpec::Sidekiq::Matchers::HaveEnqueuedJob.new worker_args }
-  let(:matcher_subject) { RSpec::Sidekiq::Matchers::HaveEnqueuedJob.new [be_a(String), be_a(Fixnum), true, be_a(Hash)] }
+  let(:matcher_subject) { RSpec::Sidekiq::Matchers::HaveEnqueuedJob.new [be_a(String), be_a(0.class), true, be_a(Hash)] }
   let(:worker) { create_worker }
   let(:worker_args) { ['string', 1, true, { key: 'value', bar: :foo, nested: [{hash: true}] }] }
   let(:active_job) { create_active_job :mailers }
