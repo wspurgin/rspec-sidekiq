2eead07b930147a3e0c1f70dd54f9cd11eaa959c
Will Spurgin <will.spurgin@stitchfix.com>
2024-04-15 10:49:08 -0500

    Tweak deprecation to clarify change
    Add silencing capability

--- lib/rspec/sidekiq/configuration.rb
+++ lib/rspec/sidekiq/configuration.rb
@@ -1,4 +1,5 @@
 require "rubygems"
+require "set"
 
 module RSpec
   module Sidekiq
@@ -14,10 +15,19 @@ module RSpec
         # Functional settings defaults
         @clear_all_enqueued_jobs = true
         @warn_when_jobs_not_processed_by_sidekiq = true
+        @silence_warnings = Set.new
       end
 
       def sidekiq_gte_7?
         Gem::Version.new(::Sidekiq::VERSION) >= Gem::Version.new("7.0.0")
+      end
+
+      def silence_warning(symbol)
+        @silence_warnings << symbol
+      end
+
+      def warn_for?(symbol)
+        !@silence_warnings.include?(symbol)
       end
     end
   end
