d44a4e259491a8f1c4a1afd4510aef8a04269422
Peter Boling <peter.boling@gmail.com>
2024-11-27 03:19:02 +0700

    Worker is dead term (#229)
    * ✏️ Fix spelling
    
    * 🐛 Queue always nil in default_retries_exhausted_message
    
    - worker is no longer used in most scenarios
      - See: https://github.com/sidekiq/sidekiq/wiki/Best-Practices#4-use-precise-terminology
    - keys are string, symbol lookups fail
    - Add test

--- lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
+++ lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
@@ -10,15 +10,15 @@ module Sidekiq
 
       def default_retries_exhausted_message
         {
-          'queue' => get_sidekiq_options[:worker],
+          'queue' => get_sidekiq_options['queue'],
           'class' => name,
           'args' => [],
-          'error_message' => 'An error occured'
+          'error_message' => 'An error occurred'
         }
       end
 
       def default_retries_exhausted_exception
-        StandardError.new('An error occured')
+        StandardError.new('An error occurred')
       end
     end
   end
